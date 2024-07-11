6007f0769b75fa753154b46eb8b61a1f7d1d6b41
JinGeun Gi <kjg5623@gmail.com>
2024-07-11 13:33:58 +0900

    initial code

---
+++ mail_send.py
@@ -0,0 +1,10 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+from dotenv import load_dotenv
+
+def send_email(filename, send_mail):
+    pass
---
+++ zip_region.py
@@ -0,0 +1,7 @@
+import os
+from zipfile import ZipFile
+from datetime import datetime
+
+def get_region_name(file):
+    # 파일명에서 지역명 추출
+    pass
